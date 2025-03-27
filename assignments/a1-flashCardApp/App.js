038829dc716655390c49e68ae2b4f007ae8758b1
nodzperez <146012330+nodzperez@users.noreply.github.com>
2025-03-27 14:12:44 -0600

    Added index tracking and answer toggle function.

--- assignments/a1-flashCardApp/App.js
+++ assignments/a1-flashCardApp/App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
 
 const flashCards = [
   {
@@ -17,6 +18,13 @@ const flashCards = [
 ];
 
 export default function App() {
+  const [index, setIndex] = useState(0);
+  const [showAnswer, setShowAnswer] = useState(false);
+
+  const nextCard = () => {
+    setIndex((index + 1) % flashcards.length);
+    setShowAnswer(false);
+  };
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
